51d532491c9f47d128823cee2dc65cceede2f1d4
Meletis <melnikolaidis86@gmail.com>
2019-09-04 01:03:14 +0300

    Initial commit cleared layout, added tachyons, boilerplate setup fetching dummy users

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            robots: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}));
+    }
+
+    render() {
+        const { robots } = this.state;
+        return !robots.length ?
+            <h1 className='f1 white-90 tc'>Loading</h1> :
+            (
+                <div className="App">
+                    {
+                        robots.map(robot => {
+                            return <h1 key={robot.id}>{robot.name}</h1>
+                        })
+                    }
+                </div>
+            );
+    }
 }
 
 export default App;
