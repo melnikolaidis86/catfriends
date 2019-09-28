f666be80675c0c6308796a49aab828e08ef8e7ed
Meletis <melnikolaidis86@gmail.com>
2019-09-28 13:24:29 +0300

    Applied middlewares for logger and async redux for fetching cat users

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,51 +5,45 @@ import CardList from "../components/card-list/CardList.component";
 import Scroll from "../components/scroll/scroll.component";
 import SearchBox from "../components/search-box/SearchBox.component";
 
-import {setSearchField} from "../redux/actions";
+import {setSearchField, requestRobots} from "../redux/actions";
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onSearchChange: event => dispatch(setSearchField(event.target.value))
+        onSearchChange: event => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     }
 };
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            robots: []
-        }
-    }
-
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({robots: users}));
+        this.props.onRequestRobots()
     }
-
 
     render() {
-        const { robots } = this.state;
-        const { searchField, onSearchChange } = this.props;
+        const { onSearchChange, searchField, robots, isPending } = this.props;
         const filteredCats = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
-        return !filteredCats.length ?
-            <h1 className='f1 white-90 tc'>Loading</h1> :
-            (
+        return (
                 <div className="tc">
                     <h1 className="f1 white-90">CatFriends</h1>
                     <SearchBox handleChange={ onSearchChange } />
                     <Scroll>
-                        <CardList robots={ filteredCats } />
+                        {
+                            isPending ?
+                                <h1 className='f1 white-90 tc'>Loading</h1> :
+                                <CardList robots={ filteredCats } />
+                        }
                     </Scroll>
                 </div>
             );
