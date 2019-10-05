ee025d5c0a57cfcad60c835e876faa5a1b3a5bce
Meletis <melnikolaidis86@gmail.com>
2019-10-06 02:52:05 +0300

    Updates

--- precache-manifest.f9511001759d0e25d0b7db99ecd23313.js
+++ precache-manifest.f9511001759d0e25d0b7db99ecd23313.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d03c57c254dce8d8a445d80f500e6649",
+    "revision": "0cb02991abdd218375875a4dce6d8a7b",
     "url": "/catfriends/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/catfriends/static/css/2.31dafeb2.chunk.css"
   },
   {
-    "revision": "fc3daa8878d61c502e3e",
+    "revision": "02c5ac3d691a55c238d1",
     "url": "/catfriends/static/css/main.1871a46f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/catfriends/static/js/2.00875e8f.chunk.js"
   },
   {
-    "revision": "fc3daa8878d61c502e3e",
-    "url": "/catfriends/static/js/main.3e8e938c.chunk.js"
+    "revision": "02c5ac3d691a55c238d1",
+    "url": "/catfriends/static/js/main.01e463e9.chunk.js"
   },
   {
     "revision": "2bfa4c7422bd26d00f43",
